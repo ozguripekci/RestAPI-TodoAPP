eb2e65cdb739c8f10924600ec3ebc4836bd55a1c
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-08-31 21:51:05 +0200

    views.py

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
-from .views import home, hello_world, todoList, todoCreate, todoListCreate, todoUpdate, todoDelete
+from .views import home, hello_world, todoList
 
 
 urlpatterns = [
     path('', home),
     path('hello/', hello_world),
-    path('todoList/', todoList),
-    path('todoCreate/', todoCreate),
-    path('todoListCreate/', todoListCreate),
-    path('todoUpdate/<int:pk>/', todoUpdate),
-    path('todoDelete/<int:pk>/', todoDelete),
-    
+    path('todoList/', todoList),  
 ]
