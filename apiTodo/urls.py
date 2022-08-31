5f59f35c2343c972926cbea75a3d83165920057b
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-09-01 00:59:10 +0200

    Status Codes

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, hello_world, todoList
+from .views import home, hello_world, todoList, todoCreate, todoListCreate
 
 
 urlpatterns = [
     path('', home),
     path('hello/', hello_world),
-    path('todoList/', todoList),  
+    path('todoList/', todoList),
+    path('todoCreate/', todoCreate),
+    path('todoListCreate/', todoListCreate),
 ]
--- apiTodo/views.py
+++ apiTodo/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from .models import Todo
 from .serializers import TodoSerializer
+from rest_framework import status
 
 def home(request):
     return HttpResponse(
@@ -24,3 +25,33 @@ def todoList(request):
     serializer = TodoSerializer(queryset, many=True)
     print(serializer)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def todoCreate(request):
+    serializer = TodoSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def todoListCreate(request):
+    if request.method == "GET":
+        queryset = Todo.objects.all()
+        serializer = TodoSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "POST":
+        serializer = TodoSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['PUT'])
+def todoUpdate(request,pk):
+        queryset = Todo.objects.get(id=pk)
