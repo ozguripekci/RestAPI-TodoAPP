2e75fa0bec9c77fd32379dc0d18f9f28765d4aa2
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-09-01 03:42:56 +0200

    Class based views

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
-from .views import home, hello_world, todoList, todoCreate, todoListCreate, todoUpdate, todoDelete
-
+#! Function based urls
+# from .views import home, hello_world, todoList, todoCreate, todoListCreate, todoUpdate, todoDelete
+#! Class based urls
+from .views import home, TodoList, TodoDetail
 
 urlpatterns = [
     path('', home),
-    path('hello/', hello_world),
-    path('todoList/', todoList),
-    path('todoCreate/', todoCreate),
-    path('todoListCreate/', todoListCreate),
-    path('todoUpdate/<int:pk>/', todoUpdate),
-    path('todoDelete/<int:pk>/', todoDelete),
+
+    #! Function based views
+    #path('hello/', hello_world),
+    #path('todoList/', todoList),
+    #path('todoCreate/', todoCreate),
+    #path('todoListCreate/', todoListCreate),
+    #path('todoUpdate/<int:pk>/', todoUpdate),
+    #path('todoDelete/<int:pk>/', todoDelete),
+
+    #! Class based views
+    path('list/', TodoList.as_view()),
+    path('detail/<int:id>/', TodoDetail.as_view()),
 ]
--- apiTodo/views.py
+++ apiTodo/views.py
@@ -4,7 +4,7 @@ from .models import Todo
 from .serializers import TodoSerializer
 from rest_framework import status
 from rest_framework.response import Response
-
+from rest_framework.views import APIView
 def home(request):
     return HttpResponse(
         '<center><h1 style="background-color:powderblue;">Welcome to ApiTodo</h1></center>'
@@ -18,15 +18,15 @@ from rest_framework.response import Response
 def hello_world(request):
     return Response({"message": "Hello, world!"})
 
+#! Function Based Views
 
-@api_view(['GET'])
+""" @api_view(['GET'])
 def todoList(request):
     queryset = Todo.objects.all()
     print(queryset)
     serializer = TodoSerializer(queryset, many=True)
     print(serializer)
     return Response(serializer.data)
-
 
 @api_view(['POST'])
 def todoCreate(request):
@@ -35,7 +35,6 @@ def todoCreate(request):
         serializer.save()
 
     return Response(serializer.data)
-
 
 @api_view(['GET', 'POST'])
 def todoListCreate(request):
@@ -83,4 +82,45 @@ def todoDelete(request, pk):
     querset =  Todo.objects.get(id = pk)
     if request.method == "DELETE":
         querset.delete()
-        return Response("Item Deleted")
+        return Response("Item Deleted") """
+
+
+#! Class Based Views
+
+
+class TodoList(APIView):
+
+    def get(self, request):
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+
+class TodoDetail(APIView):
+
+    def get (self, request,id):
+        todo = Todo.objects.get(id=id)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+    
+    def put(self, request,id):
+        todo = Todo.objects.get(id=id)
+        serializer = TodoSerializer(instance=todo, data=serializer.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request,id):
+        todo = Todo.objects.get(id=id)
+        todo.delete()
+        data = {
+            'message': 'Todo is deleted'
+        }
+        return Response(data, status=status.HTTP_204_NO_CONTENT)
